b666bbb9ccb9b252aea7501acecca9220b2c82c2
unays7 <unays.angus@gmail.com>
2023-03-06 14:13:42 +0000

    CHECK

---
+++ Header Files/RandomFunctions.hpp
@@ -0,0 +1,36 @@
+#pragma once
+//#include "functions.h"
+
+#define randfloat ((float)rand() / (float)RAND_MAX)
+#define randfloat2 ((2*randfloat) - 1.f)
+#define UNARY 0
+#define BINARY 1
+#define TERMINALINDEX 2
+#define TERMINALVALUE 3
+
+
+
+class RandomFunction{
+
+int type;
+int index;
+float value;
+bool vectorize;
+int unaryFuncCount;
+int binaryFuncCount;
+RandomFunction *lhs;
+RandomFunction *rhs;
+int v;
+int g;
+
+public:
+    RandomFunction(int count, int depth, bool vectorize){
+    }
+
+
+
+
+
+};
+
+
